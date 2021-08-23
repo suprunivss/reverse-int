762a8c88eb99e69e2ed9ba0602f2eaf0a5fd0c5c
suprunivss <suprunivss@gmail.com>
2021-08-23 22:45:59 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n <= 0) {
+        n = -n
+    }
+    let text = n;
+    let ReverseText = text.toString().split("").reverse().join("");
+    return Math.abs(ReverseText);
+};
